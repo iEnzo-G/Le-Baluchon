b60f45ea5df1a2525e3938c67cffcce5375a38df
Enzo Gammino <96486074+iEnzo-G@users.noreply.github.com>
2022-08-16 15:12:00 +0200

    Add extension to UViewController, function to present Alert for Error

---
+++ Le Baluchon/Controller/UiViewController+Alert.swift
@@ -0,0 +1,20 @@
+//
+//  UiViewController+Alert.swift
+//  Le Baluchon
+//
+//  Created by Enzo Gammino on 16/08/2022.
+//
+
+import SwiftUI
+
+struct UiViewController_Alert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UiViewController_Alert_Previews: PreviewProvider {
+    static var previews: some View {
+        UiViewController_Alert()
+    }
+}
